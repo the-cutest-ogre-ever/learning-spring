7637331ff145a47b11eae8538b99ffbeb3d3f769
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2020-08-31 23:14:01 +0300

    postConstruct, preDestroy annotations

--- src/main/java/ru/course/spring/swampy/ClassicalMusic.java
+++ src/main/java/ru/course/spring/swampy/ClassicalMusic.java
@@ -3,10 +3,27 @@ package ru.course.spring.swampy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
+
+
+
     private ClassicalMusic() {}
+
+    @PostConstruct
+    public void init()
+    {
+        System.out.println("initialization...");
+    }
+
+    @PreDestroy
+    public void dest()
+    {
+        System.out.println("destroying...");
+    }
 
     public static ClassicalMusic getClassical() {
         return new ClassicalMusic();
@@ -16,4 +33,7 @@ public class ClassicalMusic implements Music {
     public String getSong() {
         return "Hungarian Rhapsody";
     }
+
+
+
 }
